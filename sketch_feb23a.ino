6997bfa91a53581eb97647ce20d5021848a684eb
WilliamSamson <williamsamson036@gmail.com>
2022-02-25 16:32:02 +0100

    Added wireless support to dc car

--- sketch_feb23a.ino
+++ sketch_feb23a.ino
@@ -1,19 +1,11 @@
-#define xAxis A0
-#define yAxis A1
-#define IN1 7
-#define IN2 6
-#define IN3 5
-#define IN4 4
+#define IN1 7 //left motor forward
+#define IN2 6 //left motor reverse
+#define IN3 5 //right motor forward
+#define IN4 4 //right motor reverse
 #define ENA 9
 #define ENB 3
 
 void setup() {
-  Serial.begin(9600);
-  
-  // joystick control
-  pinMode(xAxis, INPUT);
-  pinMode(yAxis, INPUT);
-
   // left motor
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -22,35 +14,31 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
+  // motor controller
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
   // inputs to the motor controller
   digitalWrite(ENA, HIGH);
   digitalWrite(ENB, HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  fowardAndBackwardMovement(analogRead(xAxis)); leftAndRightMovement(analogRead(yAxis));
-}
+  if (Serial.available()) {
+    char data = Serial.read(); // data coming from bluetooth
 
-void fowardAndBackwardMovement(int xMove) {
-  if (xMove <= 450) {
-    digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-    Serial.println("front");
-  } else if (xMove > 450 && xMove < 573) {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
-  } else {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH); digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-    Serial.println("back");
-  }
-}
-
-void leftAndRightMovement(int yMove) {
-  if (yMove <= 450) {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH); digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-    Serial.println("right");
-  } else if (yMove > 450 && yMove < 573) {
-    digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
-  } else {
-    digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-    Serial.println("left");
+    if (data == 'F') {
+      digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW); // move the motors forward
+    } else if (data == 'B') {
+      digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH); digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH); // move the motors backward
+    } else if (data == 'R') {
+      digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW); digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH); // move the left motor forward and the right motor backward
+    } else if (data == 'L') {
+      digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH); digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW); // move the left motor backward and the right motor forward
+    } else {
+      digitalWrite(IN1, LOW); digitalWrite(IN2, LOW); digitalWrite(IN3, LOW); digitalWrite(IN4, LOW); // stop all the motors
+    }
   }
 }
